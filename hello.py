7cf434ecf5f6d601b7e9d9791faab17d01b9687f
rajdawar <rajeev.dawar@gmail.com>
2015-09-10 15:34:09 -0500

    AfterCounter

--- hello.py
+++ hello.py
@@ -1,7 +1,12 @@
 import os
 import uuid
+import redis
+import json
 from flask import Flask
 
+rediscloud_service = json.loads(os.environ['VCAP_SERVICES'])['rediscloud'][0]
+credentials = rediscloud_service['credentials']
+r = redis.Redis(host=credentials['hostname'], port=credentials['port'], password=credentials['password'])
 
 app = Flask(__name__)
 my_uuid = str(uuid.uuid1())
@@ -13,7 +18,8 @@ COLOR = GREEN
 @app.route('/')
 def hello():
 
-
+    r.incr('HITCOUNT')
+    PAGEHITS = r.get('HITCOUNT')
     return """
     <html>
     <body bgcolor="{}">
@@ -21,12 +27,14 @@ def hello():
     <center><h1><font color="white">Hi, I'm GUID:<br/>
     {}</br>
 
+    {}</br>
 
     </center>
-
-    </body>
+    
+ 
+   </body>
     </html>
-    """.format(COLOR,my_uuid,)
+    """.format(COLOR,my_uuid,PAGEHITS)
 
 if __name__ == "__main__":
 	app.run(debug=True,host='0.0.0.0', port=int(os.getenv('VCAP_APP_PORT', '5000')))
